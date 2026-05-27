dfc310da098ee7cffd96b2c45357f48d1c1c2cce
CORTEZ DE LA CRUZ DIEGO (Estadías) <EST.DIEGO.CORTEZ@inegi.org.mx>
2026-05-27 16:31:55 -0600

    Avance en el diseño

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from jinja2 import ChoiceLoader, FileSystemLoader
+import mock_data
 
 app = Flask(__name__)
 
-app.jinja_loader = ChoiceLoader ([
+app.jinja_loader = ChoiceLoader([
     FileSystemLoader("components"),
     FileSystemLoader("templates")
 ])
@@ -11,6 +12,11 @@ app.jinja_loader = ChoiceLoader ([
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/verificar")
+def verificar():
+    identificacion = request.
+    #No se como seguirle aqui XDDDDDDDDDD
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ mock_data.py
@@ -0,0 +1,25 @@
+mock_usuarios = [{
+    "id": 747474,
+    "photo": "../static/styles/faces/747474.png",
+    "name": "Diego",
+    "last_names": "Cortez de la Cruz",
+    "description": "DIRECCION GENERAL DE INFORMATICA",
+    "job": "Programmer",
+},
+{
+    "id": 414141,
+    "photo": "../static/styles/faces/414141.png",
+    "name": "Frida Yadira",
+    "last_names": "Chavéz Villegas",
+    "description": "DIRECCION GENERAL DE INFORMATICA",
+    "job": "Programmer",
+},
+{
+    "id": 676767,
+    "photo": "../static/styles/faces/676767.png",
+    "name": "Christian Alejandro",
+    "last_names": "Veloz Jimenez",
+    "description": "DIRECCION GENERAL DE DISEÑO",
+    "job": "Designer",
+}
+]
